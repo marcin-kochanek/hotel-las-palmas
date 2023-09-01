56c9c9d50df7c6644a57331b296b0cbf63ac2d47
Marcin Kochanek <marcinkochanek@MacBook-Pro-Marcin.local>
2023-09-01 13:28:56 +0200

    configure project structure, add first components

---
+++ src/ui/Heading.jsx
@@ -0,0 +1,28 @@
+import { css, styled } from 'styled-components';
+
+const Heading = styled.h1`
+  ${(props) =>
+    props.as === 'h1' &&
+    css`
+      font-size: 3rem;
+      font-weight: 600;
+    `}
+
+  ${(props) =>
+    props.as === 'h2' &&
+    css`
+      font-size: 2rem;
+      font-weight: 600;
+    `}
+    
+    ${(props) =>
+    props.as === 'h3' &&
+    css`
+      font-size: 1rem;
+      font-weight: 600;
+    `}
+
+  color: var(--color-brand-500);
+`;
+
+export default Heading;
